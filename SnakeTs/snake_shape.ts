e51f363eb7af94890ca1b63c89d876716c1f0c91
Dina Goldshtein <dinazil@gmail.com>
2016-01-23 17:57:48 +0200

    Make the snake even prettier

--- SnakeTs/snake_shape.ts
+++ SnakeTs/snake_shape.ts
@@ -24,23 +24,31 @@ module SnakeTs {
             return false;
         }
         
+        private drawLink(p: IPoint, context: CanvasRenderingContext2D, board:Board): void {
+            var actualLocation = board.convertPoint({x: p.x + 0.1, y: p.y + 0.1});
+            var actualSize = board.convertLength(0.8);
+            context.beginPath();
+            context.rect(actualLocation.x, actualLocation.y, actualSize, actualSize);
+            context.closePath();
+            context.fill();
+            context.stroke();
+        }
+        
         public draw(board: Board): void {
             var context = board.DrawingContect;
             
             context.save();
+
+            context.fillStyle = "purple";
             context.strokeStyle = "black";
+            
+            this.drawLink(this._body[0], context, board);            
+
             context.fillStyle = "green";
-                
             var l = this._body.length;
-            for (var i = 0; i < l; ++i) {
+            for (var i = 1; i < l; ++i) {
                 var p = this._body[i];
-                var actualLocation = board.convertPoint({x: p.x + 0.1, y: p.y + 0.1});
-                var actualSize = board.convertLength(0.8);
-                context.beginPath();
-                context.rect(actualLocation.x, actualLocation.y, actualSize, actualSize);
-                context.closePath();
-                context.fill();
-                context.stroke();
+                this.drawLink(p, context, board);                
             }
             
             context.restore();
